2bc50a256c69638fc00aa5e616956eeae9c49b25
theadamsabra <theadamsabra@gmail.com>
2020-08-20 14:55:20 -0700

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -17,7 +17,7 @@ class Preprocess:
         self.n_fft = n_fft # Number of Fast Fourier Transforms we are performing
         self.n_mfcc = n_mfcc # Number of MFCCs we are incorperating
         self.hop_length = hop_length # How many points we're sliding over
-        self.num_chan = num_chan # Number of channels of audio in question # Number of segments we want to split all audio into
+        self.num_chan = num_chan # Number of channels of audio in question
         self.dsd_path = dsd_path # Path of DSD100
         self.input_shape = input_shape # None for now, will be tuple of shape
         self.data_path = data_path
@@ -55,16 +55,16 @@ class Preprocess:
 
         '''
         # Take MFCC of first channel and transpose for continuitity.
-        mfcc1 = librosa.feature.mfcc(audio[start:end, 0], sr = self.sr,
+        mfcc1 = librosa.feature.mfcc(audio[0, start:end], sr = self.sr,
                                     n_fft = self.n_fft, n_mfcc = self.n_mfcc, 
                                     hop_length = self.hop_length)
         # Take MFCC of second channel and transpose again.
-        mfcc2 = librosa.feature.mfcc(audio[start:end, 1], sr = self.sr,
+        mfcc2 = librosa.feature.mfcc(audio[1, start:end], sr = self.sr,
                                     n_fft = self.n_fft, n_mfcc = self.n_mfcc, 
                                     hop_length = self.hop_length)
-        # Horizontally stack the 2 MFCCs and reshape to a 3D array.
-        mfcc = np.hstack((mfcc1, mfcc2)).reshape(mfcc1.shape[1], mfcc1.shape[0], 
-                                                self.num_chan)
+        # Vertically Stack stack the 2 MFCCs and reshape to a 3D array.
+        # (26,22) -> (2,22,13); we can change this later if needed.
+        mfcc = np.vstack((mfcc1, mfcc2)).reshape(self.num_chan, mfcc1.shape[1], mfcc1.shape[0])
         return mfcc
     def save_mfcc(self, mix, target):
         '''
@@ -85,20 +85,20 @@ class Preprocess:
         # Expected value of segments in song.
         Eseg = math.ceil(mix.shape[1] / Elen)
         # Iterating through all segments of the song.
-        print(Eseg, mix.shape[1], Elen)
-        # for segment in range(Eseg):
-        #     # Start and end of slice of song
-        #     start = samp_per_segment * segment
-        #     end = start + samp_per_segment
-        #     # MFCCs of Mix:
-        #     mix_mfcc = self.multi_mfcc(mix, start, end)
-        #     # MFCCs of Vocals:
-        #     tgt_mfcc = self.save_mfcc(target, start, end)
-        #     # If the mfccs are the desired length, we use it.
-        #     if (len(mix_mfcc) and len(tgt_mfcc)) == Elen:
-        #         # Append to list for storage.
-        #         self.data['Mixture MFCC'].append(mix_mfcc.tolist())
-        #         self.data['Target MFCC'].append(tgt_mfcc.tolist())
+        for segment in range(Eseg):
+            # Start and end of slice of song
+            start = samp_per_segment * segment
+            end = start + samp_per_segment
+            # MFCCs of Mix:
+            mix_mfcc = self.multi_mfcc(mix, start, end)
+            # MFCCs of Vocals:
+            tgt_mfcc = self.multi_mfcc(target, start, end)
+            # If the mfccs are the desired length, we use it.
+            if (mix_mfcc.shape[1] and tgt_mfcc.shape[1]) == Elen:
+                # Append to list for storage.
+                self.data['Mixture MFCC'].append(mix_mfcc.tolist())
+                self.data['Target MFCC'].append(tgt_mfcc.tolist())
+        print('Song has been segmented and appended.')
 
 
     # Preprocess data.
@@ -132,7 +132,7 @@ class Preprocess:
                 mix, samplerate = librosa.load(mix_file, sr = self.sr, mono = False)
                 target, samplerate = librosa.load(target_file, sr = self.sr, mono = False)
                 self.save_mfcc(mix, target)
-                print('{} completed.'.format(song))
+                print('{} has been saved'.format(song))
         # Determine whether or not we are saving the test or train data
         if dev_test == 'Test':
             json_path = os.path.join(self.data_path, (dev_test.lower() + '.json'))
