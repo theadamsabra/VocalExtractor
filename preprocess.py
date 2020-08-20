b24efe7088dfd918c38d22c73b6c76751b87fde8
theadamsabra <theadamsabra@gmail.com>
2020-08-20 14:27:59 -0700

    update

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,6 @@ import librosa
 import numpy as np
 import math
 import json
-
 '''
 There are plenty of ways to structure the preprocessing the data the way the
 directory is structured. I chose to use a preprocessing class to keep things
@@ -12,21 +11,21 @@ organized in one object.
 # Preprocessing class
 class Preprocess:
     # When initializing, we set up necessary constants needed for preprocessing.
-    def __init__(self):
-        self.sr = 44100 # Samplerate
-        self.n_fft = 2048 # Number of Fast Fourier Transforms we are performing
-        self.n_mfcc = 13 # Number of MFCCs we are incorperating
-        self.hop_length = 512 # How many points we're sliding over
-        self.num_chan = 2 # Number of channels of audio in question
-        self.num_segments = 100 # Number of segments we want to split all audio into
-        self.dsd_path = 'C:\Users\19512\Downloads\DSD100' # Path of DSD100
-        self.input_shape = None # None for now, will be tuple of shape
+    def __init__(self, target = 'vocals', sr = 44100, n_fft = 2048, n_mfcc = 13, hop_length = 512, num_chan = 2, dsd_path = 'C:\\Users\\19512\\Downloads\\DSD100\\DSD100',
+    input_shape = None, data_path = 'C:\\Users\\19512\\OneDrive\\GitHub\\Audio-Source-Separation\\Audio-Source-Separation--Undergraduate-Thesis-\\data'):
+        self.sr = sr # Samplerate
+        self.n_fft = n_fft # Number of Fast Fourier Transforms we are performing
+        self.n_mfcc = n_mfcc # Number of MFCCs we are incorperating
+        self.hop_length = hop_length # How many points we're sliding over
+        self.num_chan = num_chan # Number of channels of audio in question # Number of segments we want to split all audio into
+        self.dsd_path = dsd_path # Path of DSD100
+        self.input_shape = input_shape # None for now, will be tuple of shape
+        self.data_path = data_path
+        self.target = target
         self.data = {
             'Mixture MFCC': [],
             'Target MFCC': []
         }
-        self.data_path = 'C:\Users\19512\OneDrive\GitHub\Audio-Source-Separation\Audio-Source-Separation--Undergraduate-Thesis-\data' # Path of json files for storage
-        self.target = 'vocals' # Target in question
     def multi_mfcc(self, audio, start, end):
         '''
         The main objective of multi_mfcc is to take the MFCC of a multi-channel
@@ -64,10 +63,9 @@ class Preprocess:
                                     n_fft = self.n_fft, n_mfcc = self.n_mfcc, 
                                     hop_length = self.hop_length)
         # Horizontally stack the 2 MFCCs and reshape to a 3D array.
-        mfcc = np.hstack((mfcc1.T, mfcc2.T)).reshape(mfcc1.shape[0], mfcc1.shape[1], 
+        mfcc = np.hstack((mfcc1, mfcc2)).reshape(mfcc1.shape[1], mfcc1.shape[0], 
                                                 self.num_chan)
         return mfcc
-
     def save_mfcc(self, mix, target):
         '''
         This function segments the mix and targets and takes its MFCC.
@@ -80,25 +78,29 @@ class Preprocess:
         target: (array)
         The numpy array that contains the target in question.
         '''
-        # This is wrong. Find number of samples in segment then go from there.
-        samp_per_segment = int(mix.shape[0] / self.num_segments)
-        # Expected value of len of segments. We'll ignore if it's shorter
+        # Number of samples per segment. A quarter of the sample rate is a quarter of a second
+        samp_per_segment = int(self.sr / 4)
+        # Expected value of length of segments. We'll ignore if it's shorter
         Elen = math.ceil(samp_per_segment / self.hop_length)
+        # Expected value of segments in song.
+        Eseg = math.ceil(mix.shape[1] / Elen)
         # Iterating through all segments of the song.
-        for segment in range(self.num_segments):
-            # Start and end of slice of song
-            start = samp_per_segment * segment
-            end = start + samp_per_segment
-            # MFCCs of Mix:
-            mix_mfcc = self.multi_mfcc(mix, start, end)
-            # MFCCs of Vocals:
-            tgt_mfcc = self.multi_mfcc(target, start, end)
-            # If the mfcc is the desired length, we use it.
-            if (len(mix_mfcc) and len(tgt_mfcc)) == Elen:
-                # Append to list for storage.
-                self.data['Mixtures MFCC'].append(mix_mfcc.tolist())
-                self.data['Target MFCC'].append(tgt_mfcc.tolist())
-    
+        print(Eseg, mix.shape[1], Elen)
+        # for segment in range(Eseg):
+        #     # Start and end of slice of song
+        #     start = samp_per_segment * segment
+        #     end = start + samp_per_segment
+        #     # MFCCs of Mix:
+        #     mix_mfcc = self.multi_mfcc(mix, start, end)
+        #     # MFCCs of Vocals:
+        #     tgt_mfcc = self.save_mfcc(target, start, end)
+        #     # If the mfccs are the desired length, we use it.
+        #     if (len(mix_mfcc) and len(tgt_mfcc)) == Elen:
+        #         # Append to list for storage.
+        #         self.data['Mixture MFCC'].append(mix_mfcc.tolist())
+        #         self.data['Target MFCC'].append(tgt_mfcc.tolist())
+
+
     # Preprocess data.
     def preprocess(self, dev_test):
         '''
@@ -116,14 +118,13 @@ class Preprocess:
         '''
         mixtures = 'Mixtures'
         source = 'Sources'
-        mix_path = self.dsd_path + mixtures + '/' + dev_test
-        target_path = self.dsd_path + source + '/' + dev_test
-
+        mix_path = os.path.join(self.dsd_path, mixtures, dev_test)
+        target_path = os.path.join(self.dsd_path, source, dev_test)
         for dirpath, dirnames, filenames in sorted(os.walk(mix_path)):
             # Ensure we're not at the root level
             if dirpath is not mix_path:
                 # Get song name
-                song = dirpath.split('/')[-1]
+                song = dirpath.split('\\')[-1]
                 # Paths for files
                 mix_file = os.path.join(mix_path, song, mixtures[:-1].lower() + '.wav')
                 target_file = os.path.join(target_path, song, self.target + '.wav')
@@ -131,6 +132,7 @@ class Preprocess:
                 mix, samplerate = librosa.load(mix_file, sr = self.sr, mono = False)
                 target, samplerate = librosa.load(target_file, sr = self.sr, mono = False)
                 self.save_mfcc(mix, target)
+                print('{} completed.'.format(song))
         # Determine whether or not we are saving the test or train data
         if dev_test == 'Test':
             json_path = os.path.join(self.data_path, (dev_test.lower() + '.json'))
@@ -139,9 +141,10 @@ class Preprocess:
         # Save data as json file
         with open(json_path, 'w') as json_file:
             json.dump(self.data, json_file, indent = 4)
+            print('Writing in the json for {} has been complete.'.format(dev_test))
 
 # Main function
 if __name__ == "__main__":
-    pre = Preprocessing()
-    for dev_test in ['Dev', 'Test']:
+    pre = Preprocess()
+    for dev_test in ['Test', 'Dev']:
         pre.preprocess(dev_test)
