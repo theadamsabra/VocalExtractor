f1aa776d60ed552038d8f54a58d1c8e423d480f8
theadamsabra <theadamsabra@gmail.com>
2020-09-10 12:01:55 -0700

    drafting model

--- model.py
+++ model.py
@@ -1,8 +1,15 @@
 import h5py
 import numpy as np
 import os
+# Clean this up later
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.nn import leaky_relu, relu
+from tensorflow.keras.layers import BatchNormalization, Dropout
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose
 
-# Functions for bringing in the test/train data
+# Functions for bringing in the test/train data - might be imported as
+# separate file.
 def load_in(path, key):
     # Load in x_test, x_train, y_test, y_train
     with h5py.File(path, 'r') as f:
@@ -17,15 +24,37 @@ def train_test_set(train_path, test_path):
     return x_train, y_train, x_test, y_test
 
 # Custom loss function for VocalUNet
-def u_net_loss():
+class u_net_loss(tf.keras.losses.Loss):
     pass
 
-class VocalUNet:
-    def __init__(self, batch_size, num_classes, epochs):
-        self.model = None
-        self.batch_size = batch_size
-        self.num_classes = num_classes
-        self.epochs = epochs
+class VocalUNet(tf.keras.Model):
+    def __init__(self, inputs, kernel_size, strides):
+        super(VocalUNet, self).__init__()
+        # Initialize parameters
+        self.inputs = inputs
+        self.kernel_size = kernel_size
+        self.strides = strides
+        # Initialize network - Convolve down to 512 filters.
+        self.conv1 = BatchNormalization(Conv2D(inputs = self.inputs,
+        filters = 16, kernel_size = self.kernel_size, strides = self.strides,
+        activation = leaky_relu))
+        self.conv2 = BatchNormalization(Conv2D(inputs = self.inputs,
+        filters = 32, kernel_size = self.kernel_size, strides = self.strides,
+        activation = leaky_relu))
+        self.conv3 = BatchNormalization(Conv2D(inputs = self.inputs,
+        filters = 64, kernel_size = self.kernel_size, strides = self.strides,
+        activation = leaky_relu))
+        self.conv4 = BatchNormalization(Conv2D(inputs = self.inputs,
+        filters = 128, kernel_size = self.kernel_size, strides = self.strides,
+        activation = leaky_relu))
+        self.conv5 = BatchNormalization(Conv2D(inputs = self.inputs,
+        filters = 256, kernel_size = self.kernel_size, strides = self.strides,
+        activation = leaky_relu))
+        self.conv6 = BatchNormalization(Conv2D(inputs = self.inputs,
+        filters = 512, kernel_size = self.kernel_size, strides = self.strides,
+        activation = leaky_relu))
+        # Deconvolve layers.
+        pass
     # Function to train the model
     def train_model(self, x_train, y_train):
         # if x_train.shape == y_train.shape:
