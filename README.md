227a847326e122d5937095d5e903e94d88350a3a
theadamsabra <theadamsabra@gmail.com>
2020-10-20 19:11:59 -0700

    update

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# VocalExtractor (Undergraduate Thesis)
+# Vocal Extractor
+## Adam Sabra's Undergraduate Research Project
 
 ## Where is the project at now?
-I have preprocessed all the train and test data. I am currently building [a U-Net architetcure for Vocal Extraction.](https://pdfs.semanticscholar.org/83ea/11b45cba0fc7ee5d60f608edae9c1443861d.pdf)
-
+I built [a U-Net architetcure for Vocal Extraction.](https://pdfs.semanticscholar.org/83ea/11b45cba0fc7ee5d60f608edae9c1443861d.pdf)
+I am currently debugging and training the model on the cloud.
 ### Checklist:
 - [x] Dataset Retrieval (DSD100)
 - [x] EDA + Data Transformation
 - [x] Preprocess all songs
 - [x] Build U-Net for Vocal Extraction
 - [ ] Train on Cloud.
-- [ ] Find new model for comparison
-- [ ] Formalize findings and write paper
+- [ ] Build README for others to use efficiently
+- [ ] Create web app to extract vocals from user input.
--- load.py
+++ load.py
@@ -1,26 +1,26 @@
-# Quick functions for loading in data.
-# These will be imported and used in each model
+'''
+To use these functions are simple. In fact, you only need train_test_set,
+the path to the training data, and the path to the test data.
+
+from load import train_test_set as tts
+x_train, y_train, x_test, y_test = tts(train_path, test_path)
+
+It's that simple.
+'''
 import h5py
 import os
 import numpy as np
 
 def load_in(path, key):
-    # Load in x_test, x_train, y_test, y_train
+    # Load in data from .hdf5 files
     with h5py.File(path, 'r') as f:
-        array = np.array(f[key], dtype='float64')
-    return array # Might have to add .reshape
+        array = np.array(f[key], dtype='float32')
+    return array.reshape(array.shape[0], array.shape[2], array.shape[1], 1)
 
 def train_test_set(train_path, test_path):
+    # Load the train and test variables
     x_train = load_in(train_path, 'mixture')
     y_train = load_in(train_path, 'target')
     x_test = load_in(test_path, 'mixture')
     y_test = load_in(test_path, 'target')
     return x_train, y_train, x_test, y_test
-
-def main():
-    train_path = os.path.abspath('data/Dev.hdf5')
-    test_path = os.path.abspath('data/Test.hdf5')
-    x_train, y_train, x_test, y_test = train_test_set(train_path, test_path)
-
-if __name__ == '__main__':
-    main()
